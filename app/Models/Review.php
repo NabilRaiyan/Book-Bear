ed55df9c7238fa9db68d9fa96af6a34ca82e7908
NabilRaiyan <rraiyan77@gmail.com>
2024-05-29 09:32:31 +0600

    adding seeder to the db

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = ['review', 'rating'];
     public function book(){
         // defining each review belong to one book
         return $this->belongsTo(Book::class);
