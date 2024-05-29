ae341aa24dac6729452f389c5056baad20e2304a
NabilRaiyan <rraiyan77@gmail.com>
2024-05-29 07:38:07 +0600

    Creating fake db for test

--- database/migrations/2024_05_29_011614_create_books_table.php
+++ database/migrations/2024_05_29_011614_create_books_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
             $table->timestamps();
         });
     }
--- database/migrations/2024_05_29_011630_create_reviews_table.php
+++ database/migrations/2024_05_29_011630_create_reviews_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->string('review');
+            $table->unsignedTinyInteger('rating');
+
             $table->timestamps();
         });
     }
