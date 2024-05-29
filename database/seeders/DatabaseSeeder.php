587aed2737110693178b7469cb133e6bd35bf1ab
NabilRaiyan <rraiyan77@gmail.com>
2024-05-29 09:21:43 +0600

    adding seeder to the db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Review;
+use App\Models\Book;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // generate 100 books with each book have atleast 5 and max 30 reviews
+        Book::factory(33)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Book::factory(33)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function($book){
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
