51c668b8d7e0fee696565462223834089c124e77
NabilRaiyan <rraiyan77@gmail.com>
2024-05-29 10:02:48 +0600

    adding local query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -11,5 +11,11 @@ class Book extends Model
     public function review(){
         // creating oneToMany relation with book schema
         return $this->hasMany(Review::class);
+    }
+
+    // query for search book
+    public function scopeSearchTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
