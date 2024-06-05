42dd3e34a0cf9c7425d7c11b3919e67f8f0530bc
NabilRaiyan <rraiyan77@gmail.com>
2024-06-05 20:34:26 +0600

    showing books

--- resources/views/Books/index.blade.php
+++ resources/views/Books/index.blade.php
@@ -2,5 +2,38 @@
 
 
 @section('content')
-Hello there
+    <h1 class="mt-10 mb-10 text-2xl">Books</h1>
+    <form></form>
+
+    <ul>
+        @forelse ($books as $book )
+        <li class="mb-4">
+            <div class="book-item">
+                <div
+                class="flex flex-wrap items-center justify-between">
+                <div class="w-full flex-grow sm:w-auto">
+                    <a href="#" class="book-title">Book Title</a>
+                    <span class="book-author">by Piotr Jura</span>
+                </div>
+                <div>
+                    <div class="book-rating">
+                    3.5
+                    </div>
+                    <div class="book-review-count">
+                    out of 5 reviews
+                    </div>
+                </div>
+                </div>
+            </div>
+        </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="#" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endforelse
+        
+    </ul>
 @endsection
