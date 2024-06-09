58a14727603928c76a28724eff3c9376c720a390
NabilRaiyan <rraiyan77@gmail.com>
2024-06-09 19:26:55 +0600

    invalidated cache of queries

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
-
 class Book extends Model
 {
     use HasFactory;
