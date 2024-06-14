43143456a06d3939fa04ac075d0eddbb69c32719
NabilRaiyan <rraiyan77@gmail.com>
2024-06-14 20:50:18 +0600

    adding update route in  doctor folder

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+// Book class
 class Book extends Model
 {
     use HasFactory;
