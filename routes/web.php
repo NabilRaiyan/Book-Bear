725f91c05dcb26e406d79fd101615604859ee817
NabilRaiyan <rraiyan77@gmail.com>
2024-06-16 17:05:42 +0600

    creating reviws controller and adding book reviews route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-// registering routes
+// registering book routes
 Route::resource('books', BookController::class);
+
+// register reviews route
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only('create', 'store');
